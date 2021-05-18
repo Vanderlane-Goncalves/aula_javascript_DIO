be057cd094a1d0284d9a16bbd625670fa0e2137e
Vanderlane-Goncalves <62728154+Vanderlane-Goncalves@users.noreply.github.com>
2021-05-18 02:29:52 -0300

    demonstração
    De algumas funções do js

---
+++ js/main.js
@@ -0,0 +1,29 @@
+var fruta = [{nome:"maça", cor:"vermalha"},{nome:"Uva",cor:"Roxa"}];
+console.log(fruta[1].nome);
+/*
+var fruta = {nome:"maça", cor:"vermalha"};
+console.log(fruta.nome);
+*/
+
+
+/*var nome = "Vanderlane Gonçalves";
+var idade = 29 ;
+var idade2 = 10;
+var frase ="Japão é o melhor time do mundo"
+alert("Bem Vindo " + nome +);
+alert(idade + idade2);
+console.log(nome);
+console.log(idade+ idade2);
+console.log(frase.replace("Japão","Brasil"))
+
+var lista = ["maça","pêra","laranja"];
+console.log(lista);
+//lista.push("Uva");
+//console.log(lista);
+//lista.pop(lista[0]);
+//console.log(lista)
+//console.log(lista[0])
+//alert(lista[0])
+
+console.log(lista.toString())
+console.log(lista.join(" - "))*/
