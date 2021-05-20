d60e26a76268c91edb82c19bf8265e8680566fbd
Vanderlane-Goncalves <62728154+Vanderlane-Goncalves@users.noreply.github.com>
2021-05-19 23:44:06 -0300

    Estudo de funções

--- js/main.js
+++ js/main.js
@@ -1,6 +1,26 @@
 
+function soma(n1,n2){
+    return n1 + n2;
+};
+
+function validaIdade(idade){
+    var validar;
+    if (idade >=18){
+        validar = true
+    }else{
+        validar = false
+    }
+    return validar;
+}
+
+var idade = prompt("Qual é sua idade");
+console.log(validaIdade(idade));
+
+
+/*
 var d = new Date();
 alert(d.getMonth()+1);
+*/
 
 /*
 var count;
